30d0531035520f4fde7e2167f3447ec7ec31e56f
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-11-21 03:42:39 -0700

    condense database schema into a single file

---
+++ src/db/schema.ts
@@ -0,0 +1,34 @@
+import { pgTable as table } from "drizzle-orm/pg-core";
+import * as pg from "drizzle-orm/pg-core";
+
+export const users = table("account", {
+	id: pg.serial().primaryKey(),
+	email: pg.text().unique().notNull(),
+	password: pg.text().notNull(),
+	name: pg.text().notNull(),
+	createdAt: pg.timestamp("created_at").notNull().defaultNow(),
+	updatedAt: pg.timestamp("created_at").notNull().defaultNow(),
+});
+
+export const bands = table("band", {
+	id: pg.serial().primaryKey(),
+	name: pg.text().notNull(),
+	createdAt: pg.timestamp("created_at").notNull().defaultNow(),
+	updatedAt: pg.timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const userBands = table("account_band", {
+	accountID: pg.integer("account_id").references(() => users.id),
+	bandID: pg.integer("band_id").references(() => bands.id),
+	admin: pg.boolean().notNull().default(false),
+	createdAt: pg.timestamp("created_at").notNull().defaultNow(),
+	updatedAt: pg.timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const shows = table("show", {
+	id: pg.serial().primaryKey(),
+	bandID: pg.integer("band_id").references(() => bands.id),
+	// TODO: wtf kind of data should we be actually storing about shows?
+	createdAt: pg.timestamp("created_at").notNull().defaultNow(),
+	updatedAt: pg.timestamp("updated_at").notNull().defaultNow(),
+});
